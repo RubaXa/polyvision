a69e8d5bf192e6275efefd8aa48abb6bfa4d143d
Lebedev Konstantin <ibnrubaxa@gmail.com>
2019-12-15 13:43:22 +0300

    * fixed

--- src/translate.ts
+++ src/translate.ts
@@ -76,7 +76,7 @@ export function translate(
 }
 
 function normalizeText(val: string) {
-	if (/^[A-Z\d\s.?!*-]+$/.test(val)) {
+	if (/^[A-Z\d\s."',:?!*-]+$/.test(val)) {
 		return val.charAt(0).toUpperCase() + val.slice(1).toLowerCase();
 	}
 
